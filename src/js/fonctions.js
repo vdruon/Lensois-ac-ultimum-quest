4fcc69ffc295c66b05ea89fb07f8eca0058caae9
Alexandre Durrande <alexandre.durrande@epfedu.fr>
2024-03-04 15:11:01 +0100

    Validation

--- src/js/fonctions.js
+++ src/js/fonctions.js
@@ -2,7 +2,7 @@ export function doNothing() {
     // cette fonction ne fait rien.
     // c'est juste un exemple pour voir comment mettre une fonction
     // dans un fichier et l'utiliser dans les autres
-}
+} 
 
 
 export function doAlsoNothing() {
